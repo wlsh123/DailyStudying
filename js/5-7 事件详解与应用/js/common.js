add583e7963d6376b55611e5a02be9dc29486438
wlsh <975135062@qq.com>
2020-07-10 18:08:30 +0800

    获取页面滚出去的距离和pageX重写

---
+++ js/5-7 事件详解与应用/js/common.js
@@ -0,0 +1,18 @@
+/**
+ * 获取页面滚动出去的距离
+ */
+function getScroll(){
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    var scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
+    return {scrollTop:scrollTop, scrollLeft: scrollLeft}
+}
+
+/**
+ * 获取鼠标在页面上的位置
+ */
+function getPage(e){
+    let e = e || window.event;
+    var pageX = e.pageX || e.clientX + getScroll().scrollLeft;
+    var pageY = e.pageY || e.clientY + getScroll().scrollTop;
+    return {pageX:pageX, pageY:pageY}
+}
