6760b94ddca103da35ac8ffe1d7e9db3ef1ac034
wls123 <975135062@qq.com>
2020-07-14 21:06:09 +0800

    getTextSync同步响应

--- js/脚本化HTTP/getText.js
+++ js/脚本化HTTP/getText.js
@@ -4,14 +4,28 @@
 
 //模拟监听readystatechange事件。
 function getText(url, callback) {
-    var request = new XMLHttpRequest();
-    request.open("GET", url);
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            var type = request.getResponseHeader("Content-Type");
-            if (type.match(/^text/))
-                callback(request.responseText);
-        }
-    };
-    request.send(null);
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      var type = request.getResponseHeader("Content-Type");
+      if (type.match(/^text/)) callback(request.responseText);
+    }
+  };
+  request.send(null);
+}
+
+/**
+ * 同步响应
+ */
+function getTextSync(url) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url, false);
+  request.send(null);
+  if (request.status !== 200) throw new Error(request.statusText);
+
+  var type = request.getResponseHeader("Content-Type");
+  if (!type.match(/^text/))
+    throw new Error("Excepted textual response; got: " + type);
+  return request.responseText;
 }
